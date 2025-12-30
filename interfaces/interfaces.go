097bce9229c6c5e75526eb380d9a7f79f423bcbf
gonzalomdvc <gonzalo.mdvc@gmail.com>
2025-12-30 17:20:16 +0100

    Disable vet for testing.

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -16,6 +16,9 @@ type Finding struct {
 	Check    Check
 }
 
+// Position represents a specific location in a source file. Offsets in chars.
+// Note: this might not match the column and line you see on your text editor.
+// This is because tabs are counted as a single character here.
 type Position struct {
 	Column int
 	Line   int
